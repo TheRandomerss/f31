b699c6af7edaebe7682b5e70c26d5cc04f487d9c
TheF3celess <150558281+TheF3celess@users.noreply.github.com>
2025-01-29 01:29:06 +0100

    Update traffic.js

--- traffic.js
+++ traffic.js
@@ -4,8 +4,8 @@ import { checkTz } from "./tz_px.js";
 
 const url = "https://cybertoolhub.space/";
 // Threads range
-const minThreads = 3; // Minimum threads for task execution.
-const maxThreads = 5; // Maximum threads for task execution.
+const minThreads = 5; // Minimum threads for task execution.
+const maxThreads = 15; // Maximum threads for task execution.
 let bots;
 let views = 0;
 function generateRandomNumber(min, max) {
